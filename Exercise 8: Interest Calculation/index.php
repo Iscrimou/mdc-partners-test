95b62adecd1c107999b34f35728df9a242f1e89a
Junior Oliveira <iscrimou@gmail.com>
2023-07-27 10:16:13 -0300

    interest calculator done

---
+++ Exercise 8: Interest Calculation/index.php
@@ -0,0 +1,15 @@
+<?php
+function calculateFinalValue($initialCapital, $interestRate, $investmentTime) { // function to calculate the final value of an investment
+    return number_format((1 + ($interestRate / 100) * ($investmentTime / 12)) * $initialCapital, 2, '.');
+}
+
+echo "This is a program that calculates the final value of an investment based on initial capital, interest rate, and investment time.\n"; // apresentation of the program
+
+$initialCapital = (float)readline("Digit the initial capital: "); // input for initial capital
+$interestRate = (int)readline("Now, digit the interest rate (just the number): "); // input for interest rate
+$investmentTime = (int)readline("And now, digit the investment time (in months): "); // input for investment time
+
+$result = calculateFinalValue($initialCapital, $interestRate, $investmentTime); // call the function
+echo "Result: $result\n"; // result
+
+?>
