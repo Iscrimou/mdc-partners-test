f5214a40099e4ef1317cab03dfac32d7cb1110a5
Junior Oliveira <iscrimou@gmail.com>
2023-07-27 10:22:27 -0300

    Auto message

--- Exercise 8: Interest Calculation/index.php
+++ Exercise 8: Interest Calculation/index.php
@@ -1,6 +1,6 @@
 <?php
 function calculateFinalValue($initialCapital, $interestRate, $investmentTime) { // function to calculate the final value of an investment
-    return number_format((1 + ($interestRate / 100) * ($investmentTime / 12)) * $initialCapital, 2, '.');
+    return number_format((1 + ($interestRate / 100) * ($investmentTime / 12)) * $initialCapital, 2, '.'); // calculate the final value and format it to two decimal places
 }
 
 echo "This is a program that calculates the final value of an investment based on initial capital, interest rate, and investment time.\n"; // apresentation of the program
